0fa368c4f4bf74bca4589f3de0db71ba55e205d5
Bud Parr <budparr@sonnetmedia.net>
2015-07-01 12:18:47 -0400

    Replace live reload's build-static to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,7 +63,7 @@ module.exports = function(grunt) {
       },
       sass : {
         files: ['sass/*.scss','sass/**/*.scss'],
-        tasks: ['sass', 'build'],
+        tasks: ['sass', 'build-static'],
          nonull: true,
       }
     }
